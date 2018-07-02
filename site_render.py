9146edc8ee15da3d78d72eba353960275d4758e9
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-07-02 20:31:12 +0300

    Complete the front-end.

--- site_render.py
+++ site_render.py
@@ -26,7 +26,7 @@ def sort_by_number(entry_name):
         return entry_name
 
 
-def get_article_list(folders_and_files):
+def get_list_of_article_groups(folders_and_files):
     article_list = []
     for folder in sorted(folders_and_files, key=lambda x: sort_by_number(x)):
         article_group = {}
@@ -38,11 +38,23 @@ def get_article_list(folders_and_files):
         ):
             article = {}
             file_root, file_extension = os.path.splitext(file_name)
-            article['article_name'] = file_root
+            # TODO Translate file root to russian article title.
+            # TODO A special dictionary seems to be useful.
+            article['article_title'] = file_root
             article['article_html'] = '{}.html'.format(file_root)
             article_group['article_list'].append(article)
         article_list.append(article_group)
     return article_list
+
+
+def get_context_index():
+    directories_and_files = get_directories_and_article_files()
+    article_groups = get_list_of_article_groups(directories_and_files)
+    context = {
+        'page_name': 'Main page',
+        'article_groups': article_groups
+    }
+    return context
 
 
 def get_article_content(template):
@@ -50,7 +62,7 @@ def get_article_content(template):
     article_title, _ = tail.split('.')
     with open(template.filename) as template_file:
         return {
-            'article': template_file.read(),
+            'article_content': template_file.read(),
             'article_title': article_title
         }
 
@@ -64,18 +76,14 @@ def render_article(env, template, **kwargs):
 
 
 def render_site():
-    directories_and_files = get_directories_and_article_files()
-    article_groups = get_article_list(directories_and_files)
-    context_index = {
-        'page_name': 'Main page',
-        'article_groups': article_groups
-    }
+    context_index = get_context_index()
     site = make_site(
         extensions=[MarkdownExtension],
         contexts=[
             ('index.html', context_index),
             ('.*.md', get_article_content)
-        ], rules=[('.*.md', render_article)],
+        ],
+        rules=[('.*.md', render_article)],
         outpath='static/',
         searchpath='templates/'
     )
@@ -83,15 +91,7 @@ def render_site():
 
 
 if __name__ == '__main__':
-    # render_site()
     server = Server()
     server.watch('templates/', render_site)
-    # # TODO watch for changes in markdown articles
+    # TODO watch for changes in markdown articles
     server.serve(root='static/')
-
-
-# with open('articles/0_tutorial/7_codenvy.md') as file:
-#     file_content = file.read()
-#
-# print(file_content)
-# print(type(file_content))
