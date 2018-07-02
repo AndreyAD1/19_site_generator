105e137261d4b0ef9661aef0490084844a2c2745
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-07-03 00:53:02 +0300

    Use Jinja instances only to simplify the code.

--- site_render.py
+++ site_render.py
@@ -3,6 +3,7 @@ from livereload import Server
 import os
 from jinja2_markdown import MarkdownExtension
 import json
+from jinja2 import Environment, FileSystemLoader
 
 
 def load_article_info():
@@ -11,50 +12,48 @@ def load_article_info():
     return article_info
 
 
-def get_context_index(article_info):
-    article_info['page_title'] = 'Main page'
+def get_html_filepath(article_info):
     for article in article_info['articles']:
         article_file_path = article['source']
         head, tail = os.path.split(article_file_path)
         file_name, extension = tail.split('.')
         html_file_path = '{}.html'.format(file_name)
-        article['html_file'] = html_file_path
+        article['html_filepath'] = html_file_path
     return article_info
 
 
-def get_article_content(template):
-    head, tail = os.path.split(template.name)
-    article_title, _ = tail.split('.')
-    with open(template.filename) as template_file:
-        return {
-            'article_content': template_file.read(),
-            'article_title': article_title
-        }
+def get_article_context(article_dict):
+    article_file_path = 'articles/{}'.format(article_dict['source'])
+    with open(article_file_path) as article_file:
+        article_content = article_file.read()
+    article_dict['article_content'] = article_content
+    return article_dict
 
 
-def render_article(env, template, **kwargs):
-    article_template = env.get_template('_article.html')
-    head, tail = os.path.split(template.name)
-    article_title, _ = tail.split('.')
-    output_file_name = 'static/{}.html'.format(article_title)
-    article_template.stream(**kwargs).dump(output_file_name)
+def render_main_page(article_info, jinja_environment):
+    article_info['page_title'] = 'Main page'
+    index_template = jinja_environment.get_template('index.html')
+    index_html = index_template.render(article_info)
+    with open('static/index.html', 'w', encoding='utf-8') as index_file:
+        index_file.write(index_html)
+
+
+def render_article_pages(article_info, jinja_environment):
+    for article in article_info['articles']:
+        article_context = get_article_context(article)
+        article_template = jinja_environment.get_template('_article.html')
+        article_html = article_template.render(article_context)
+        article_filepath = 'static/{}'.format(article['html_filepath'])
+        with open(article_filepath, 'w', encoding='utf-8') as article_file:
+            article_file.write(article_html)
 
 
 def render_site():
     article_json = load_article_info()
-    context_index = get_context_index(article_json)
-    print(context_index)
-    site = make_site(
-        extensions=[MarkdownExtension],
-        contexts=[
-            ('index.html', context_index),
-            ('.*.md', get_article_content)
-        ],
-        rules=[('.*.md', render_article)],
-        outpath='static/',
-        searchpath='templates/'
-    )
-    site.render()
+    article_json = get_html_filepath(article_json)
+    env = Environment(loader=FileSystemLoader('templates/'))
+    render_main_page(article_json, env)
+    render_article_pages(article_json, env)
 
 
 if __name__ == '__main__':
