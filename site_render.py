ffc5f254c89cc45ed5b40f176693c06c1161e273
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-07-02 22:13:50 +0300

    Use "config.json" only to render index.html.

--- site_render.py
+++ site_render.py
@@ -8,61 +8,12 @@ import json
 def load_article_info():
     with open('config.json', 'r', encoding='utf-8') as json_file:
         article_info = json.load(json_file)
-    print(article_info)
-    print(type(article_info))
+    return article_info
 
 
-def get_directories_and_article_files():
-    file_dict = {}
-    first_step = 0
-    for step_num, file_info in enumerate(os.walk('static/articles')):
-        if step_num == first_step:
-            continue
-        dirpath, dirnames, filenames = file_info
-        directory_name = os.path.basename(dirpath)
-        file_dict[directory_name] = filenames
-    return file_dict
-
-
-def sort_by_number(entry_name):
-    splitted_filename = entry_name.split('_', maxsplit=1)
-    first_splitted_item = splitted_filename[0]
-    try:
-        file_num = int(first_splitted_item)
-        return file_num
-    except ValueError:
-        return entry_name
-
-
-def get_list_of_article_groups(folders_and_files):
-    article_list = []
-    for folder in sorted(folders_and_files, key=lambda x: sort_by_number(x)):
-        article_group = {}
-        article_group['group_name'] = folder
-        article_group['article_list'] = []
-        for file_name in sorted(
-            folders_and_files[folder],
-            key=lambda x: sort_by_number(x)
-        ):
-            article = {}
-            file_root, file_extension = os.path.splitext(file_name)
-            # TODO Translate file root to russian article title.
-            # TODO A special dictionary seems to be useful.
-            article['article_title'] = file_root
-            article['article_html'] = '{}.html'.format(file_root)
-            article_group['article_list'].append(article)
-        article_list.append(article_group)
-    return article_list
-
-
-def get_context_index():
-    directories_and_files = get_directories_and_article_files()
-    article_groups = get_list_of_article_groups(directories_and_files)
-    context = {
-        'page_name': 'Main page',
-        'article_groups': article_groups
-    }
-    return context
+def get_context_index(article_info):
+    article_info['page_title'] = 'Main page'
+    return article_info
 
 
 def get_article_content(template):
@@ -84,8 +35,8 @@ def render_article(env, template, **kwargs):
 
 
 def render_site():
-    load_article_info()
-    context_index = get_context_index()
+    article_json = load_article_info()
+    context_index = get_context_index(article_json)
     site = make_site(
         extensions=[MarkdownExtension],
         contexts=[
@@ -100,7 +51,7 @@ def render_site():
 
 
 if __name__ == '__main__':
-    load_article_info()
+    render_site()
     # server = Server()
     # server.watch('templates/', render_site)
     # # TODO watch for changes in markdown articles
